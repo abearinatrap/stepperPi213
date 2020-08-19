e99468bb4ee2b70e9578aab4ba17421b5c923c6c
Andrew Renninger <awzrenn@gmail.com>
2020-08-19 10:53:43 -0500

    Update outletter.py

--- outletter.py
+++ outletter.py
@@ -9,21 +9,15 @@ GPIO.setwarnings(False)
 GPIO.setup(8, GPIO.OUT)
 
 
-# if __name__ == "__main__":
-#     if sys.argv[1]=="1":
-#         GPIO.output(8,1)
-#     elif sys.argv[1]=="0":
-#         GPIO.output(8,0)
-#     else:
-#         print("Bad input")
-#     GPIO.cleanup()
+if __name__ == "__main__":
+    try:
+        if sys.argv[1]=="1":
+            GPIO.output(8,1)
+        elif sys.argv[1]=="0":
+            GPIO.output(8,0)
+        else:
+            print("Bad input")
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()   
 
-try:  
-    while True:  
-        GPIO.output(8, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        time.sleep(0.5)                 # wait half a second  
-        GPIO.output(8, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        time.sleep(0.5)                 # wait half a second  
-  
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-    GPIO.cleanup()   
