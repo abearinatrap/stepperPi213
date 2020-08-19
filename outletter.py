15788cdb9840aa25852e58f963969843e8fa79aa
Andrew Renninger <awzrenn@gmail.com>
2020-08-19 10:52:21 -0500

    Update outletter.py

--- outletter.py
+++ outletter.py
@@ -8,13 +8,22 @@ GPIO.setwarnings(False)
 #gpio pins to control stepper motor
 GPIO.setup(8, GPIO.OUT)
 
-def setGPIO(modeN):
-    GPIO.output(8,modeN)
-if __name__ == "__main__":
-    if sys.argv[1]=="1":
-        setGPIO(GPIO.HIGH)
-    elif sys.argv[1]=="0":
-        setGPIO(GPIO.LOW)
-    else:
-        print("Bad input")
-    GPIO.cleanup()
+
+# if __name__ == "__main__":
+#     if sys.argv[1]=="1":
+#         GPIO.output(8,1)
+#     elif sys.argv[1]=="0":
+#         GPIO.output(8,0)
+#     else:
+#         print("Bad input")
+#     GPIO.cleanup()
+
+try:  
+    while True:  
+        GPIO.output(8, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        time.sleep(0.5)                 # wait half a second  
+        GPIO.output(8, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        time.sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup()   
